2036f08bb211250839019f327d6a7747f450de0a
nguyentanphatt <bloodomega97@gmail.com>
2024-08-28 23:37:34 +0700

    Complete Dom and events

---
+++ DOM and Events/ex01.js
@@ -0,0 +1,28 @@
+/** Implement a game rest functionality, so that the player can make a new guess!
+Your tasks:
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the 'score' and
+'secretNumber' variables
+3. Restore the initial conditions of the message, number, score and guess input
+fields
+4. Also restore the original background color (#222) and number width (15rem)*/
+let againBtn = document.querySelector('.again')
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+againBtn.addEventListener('click', function () {
+  
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+
+  
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
+
