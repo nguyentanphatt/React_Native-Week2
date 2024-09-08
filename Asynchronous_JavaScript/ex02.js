50497b5ea505ab623cfa68d1a4977d4ef265e257
H82M19\Student <Student>
2024-09-08 11:11:36 +0700

    fix function ex02

---
+++ Asynchronous_JavaScript/ex02.js
@@ -0,0 +1,23 @@
+/*For this challenge you will actually have to watch the video! Then, build the image
+loading functionality that I just showed you on the screen.
+Your tasks:
+Tasks are not super-descriptive this time, so that you can figure out some stuff by
+yourself. Pretend you're working on your own �
+PART 1
+1. Create a function 'createImage' which receives 'imgPath' as an input.
+This function returns a promise which creates a new image (use
+document.createElement('img')) and sets the .src attribute to the
+provided image path
+2. When the image is done loading, append it to the DOM element with the
+'images' class, and resolve the promise. The fulfilled value should be the
+image element itself. In case there is an error loading the image (listen for
+the'error' event), reject the promise
+3. If this part is too tricky for you, just watch the first part of the solution */
+
+function createImage(imgPath){
+    const img = document.createElement('img')
+    img.setAttribute('src',imgPath)
+    document.body.appendChild(img)
+}
+const imgLink = './dice_img.jpg'
+createImage(imgLink)
